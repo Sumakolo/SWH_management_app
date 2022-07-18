c0e79106807614a8f0a247cb8d1b1770e7239902
TormaLaszlo1997 <tormamark1997@gmail.com>
2022-07-18 17:31:19 +0200

    Repository part
    basic repository functions written

---
+++ SWH_management_app/Data/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Entities
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public int Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int BirthYear { get; set; }
+        public string BirthPlace { get; set; }
+        public string City { get; set; }
+    }
+}
